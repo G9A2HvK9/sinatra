8fb69690223d6ebbc0be95ed09b5cb31e286c0d2
Sara Riley <saralynnaz@gmail.com>
2017-04-25 12:58:05 +0100

    POSTed params

--- app.rb
+++ app.rb
@@ -10,14 +10,17 @@ get "/secret" do
   "This is a secret!"
 end
 
-
  get "/random-cat" do
    @name = ["Hendrik", 'Sara', "Michael"].sample
    erb(:index)
  end
 
- get "/named-cat" do
+ post "/named-cat" do
    p params
    @name = params[:name]
+   erb(:index)
+ end
+
+ get "/cat-naming" do
    erb(:index)
  end
